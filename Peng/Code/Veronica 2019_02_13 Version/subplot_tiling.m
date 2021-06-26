59bee702fa32917adf6fdab762e266edaee75e21
Max W <maxwn04@gmail.com>
2021-06-25 17:19:11 -0700

    files

--- Peng/Code/Veronica 2019_02_13 Version/subplot_tiling.m
+++
@@ -1,7 +0,0 @@
-function [m,n] = subplot_tiling(plotnum)
-    m = floor(sqrt(plotnum));
-    n = floor(sqrt(plotnum));
-    while(m*n<plotnum)
-        n = n+1;
-    end
-end
--- Peng/Code/create_new_csvs.m
+++
@@ -1,9 +0,0 @@
-writeto = "Z:\Shockwave\November 10\data";
-for i = 1:30
-    A = zeros(1,2);
-    filename = num2str(i) + ".csv";
-    fullfilename = fullfile(writeto, filename);
-    writematrix(A, fullfilename);
-  
-    
-end
